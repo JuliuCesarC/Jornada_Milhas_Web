e91cf991ea33b9059641930750de7596635c8735
Juliu Cesar <juliu2012@gmail.com>
2023-08-03 20:55:26 -0300

    fix: show the correct currency

--- src/components/HomePage/Destination/CardsDestination.tsx
+++ src/components/HomePage/Destination/CardsDestination.tsx
@@ -13,10 +13,12 @@ import {
 } from "@mui/material";
 import DestinationList from "../../mocks/DestinationListMock.json";
 import { useState } from "react";
-import getDestinationList, { IDestinationList } from "@/utils/getDestinationList";
+import getDestinationList, {
+  IDestinationList,
+} from "@/utils/getDestinationList";
 
 interface CardsDestinationProps {
-  dList: IDestinationList
+  dList: IDestinationList;
 }
 
 export default function CardsDestination(props: CardsDestinationProps) {
@@ -24,12 +26,15 @@ export default function CardsDestination(props: CardsDestinationProps) {
   const [pageNumber, setPageNumber] = useState<number>(
     props.dList.pageNumber + 1
   );
-  const totalPages = props.dList.totalPages
-  const pageWidth = "90%";  
+  const totalPages = props.dList.totalPages;
+  const pageWidth = "90%";
 
-  async function paginationChange(event: React.ChangeEvent<unknown>, value: number) {
+  async function paginationChange(
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) {
     const getList: IDestinationList = await getDestinationList(value - 1);
-    setDestinationList(getList.content)
+    setDestinationList(getList.content);
     setPageNumber(value);
   }
   return (
@@ -52,72 +57,74 @@ export default function CardsDestination(props: CardsDestinationProps) {
         }}
       >
         {destinationList.map((destination, index) => {
-            if (index > 5) return;
-            return (
-              <Card
-                className="card_destination"
-                sx={{
-                  maxWidth: 370,
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "space-between",
-                  border: "1px solid",
-                  borderColor: "grey.400",
-                  bgcolor: "primary.light",
-                  boxShadow: "2px 2px 7px rgba(0, 0, 0, 0.1)",
-                }}
-                key={index}
-              >
-                <CardMedia
-                  sx={{ height: 270 }}
-                  image={"data:image/png;base64,"+destination.imageOne}
-                  title={`Imagem card destino: ${destination.name}`}
-                />
-                <CardContent sx={{}}>
-                  <Typography
-                    variant="h5"
-                    fontSize="1.6rem"
-                    fontWeight="500"
-                    textAlign={"center"}
-                  >
-                    {destination.name}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    component="p"
-                    fontSize={"1rem"}
-                    fontWeight={"400"}
-                    sx={{
-                      maxHeight: { xs: 137 },
-                      overflow: "hidden",
-                      my: 2,
-                      lineHeight: "1.2rem",
-                    }}
-                  >
-                    {destination.target}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    component="p"
-                    textAlign={"center"}
-                    color="grey.800"
-                    fontSize="2rem"
-                  >
-                    {/* {destination.price} */}
-                    R$ 9347,08
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button
-                    variant="contained"
-                    sx={{ width: "95%", mx: "auto", mb: 3 }}
-                  >
-                    ver detalhes
-                  </Button>
-                </CardActions>
-              </Card>
-            );
-          })}
+          if (index > 5) return;
+          return (
+            <Card
+              className="card_destination"
+              sx={{
+                maxWidth: 370,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                border: "1px solid",
+                borderColor: "grey.400",
+                bgcolor: "primary.light",
+                boxShadow: "2px 2px 7px rgba(0, 0, 0, 0.1)",
+              }}
+              key={index}
+            >
+              <CardMedia
+                sx={{ height: 270 }}
+                image={"data:image/png;base64," + destination.imageOne}
+                title={`Imagem card destino: ${destination.name}`}
+              />
+              <CardContent sx={{}}>
+                <Typography
+                  variant="h5"
+                  fontSize="1.6rem"
+                  fontWeight="500"
+                  textAlign={"center"}
+                >
+                  {destination.name}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  component="p"
+                  fontSize={"1rem"}
+                  fontWeight={"400"}
+                  sx={{
+                    maxHeight: { xs: 137 },
+                    overflow: "hidden",
+                    my: 2,
+                    lineHeight: "1.2rem",
+                  }}
+                >
+                  {destination.target}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  component="p"
+                  textAlign={"center"}
+                  color="grey.800"
+                  fontSize="2rem"
+                >
+                  {destination.price.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  variant="contained"
+                  sx={{ width: "95%", mx: "auto", mb: 3 }}
+                >
+                  ver detalhes
+                </Button>
+              </CardActions>
+            </Card>
+          );
+        })}
       </Box>
       <Box sx={{ display: "flex", justifyContent: "center", my: 4 }}>
         <Pagination
