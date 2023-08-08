46b0fd739d697d744fa50ab5ecfbdcb14343c7a6
Juliu Cesar <juliu2012@gmail.com>
2023-08-08 08:46:40 -0300

    refactor: changing the BannerHome to the MUI components

--- src/components/HomePage/BannerHome/index.tsx
+++ src/components/HomePage/BannerHome/index.tsx
@@ -4,108 +4,72 @@ import styled from "@emotion/styled";
 import { Box, Container, Typography } from "@mui/material";
 import Image from "next/image";
 import frontBanner from "../../../../public/img/images/banner-front-home.png";
-import theme, { BreakPoints } from "../../ThemeRegistry/theme";
 
-interface BannerHomeProps {}
-
-const BannerContainer = styled.div<BreakPoints>`
-  position: relative;
-  width: 100%;
-  height: 450px;
-
+const BannerContainer = styled(Box)`
   #banner_principal_home {
-    position: absolute;
-    top: 0;
-    left: 50%;
-    width: 100%;
-    min-width: 700px;
-    height: 100%;
-    background-image: url("img/images/banner-bg-home.png");
-    background-position: center;
-    background-position-y: top;
-    background-repeat: no-repeat;
-    background-size: cover;
-
     transform: translateX(-50%);
-  }
-  h1 {
-    z-index: 10;
-    position: absolute;
-    top: 80px;
-    left: 15px;
-    font-size: 3rem;
-  }
-  #banner_front_home {
-    z-index: 1;
-    position: absolute;
-    bottom: 120px;
-    right: 15px;
-    width: 270px;
-    aspect-ratio: 1.5/1;
-    img{
-      width: 100%;
-      height: 100%;
-    }
-  }
-  @media (min-width: ${(props) => props.brp.sm + "px"}) {
-    h1{
-      top: 110px;
-      left: 25px;
-      font-size: 3.5rem;
-    }
-    #banner_front_home {
-      width: 370px;
-      bottom: 100px;
-      right: 25px;
-    }
-  }
-  @media (min-width: ${(props) => props.brp.md + "px"}) {
-    h1{
-      top: 140px;
-    }
-    #banner_front_home {
-      width: 470px;
-    }
-  }
-  @media (min-width: ${(props) => props.brp.xl + "px"}) {
-    height: 500px;
-    h1{
-      top: 160px;
-    }
-    #banner_principal_home {
-      background-position-y: bottom;
-    }
-    #banner_front_home {
-      width: 550px;
-      bottom: 80px;
-    }
-  }
-  @media (min-width: 2000px) {
-    height: 550px;
   }
 `;
 
-export default function BannerHome(props: BannerHomeProps) {
+export default function BannerHome() {
   return (
-    <BannerContainer
-      brp={theme.breakpoints.values}
-    >
-      <Box id="banner_principal_home"></Box>
+    <BannerContainer sx={{
+      position: "relative",
+      width: "100%",
+      height: {
+        xs: "420px",
+        sm: "450px",
+        md: "470px",
+        lg: "500px",
+        xl: "550px",
+      },
+    }}>
+      <Box
+        id="banner_principal_home"
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: "50%",
+          width: "100%",
+          height: "100%",
+          backgroundImage: "url(img/images/banner-bg-home.png)",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundPositionY: { xs: "top", xl: "bottom" },
+          backgroundSize: "cover",
+        }}
+      ></Box>
       <Container sx={{ position: "relative", height: "100%" }}>
         <Typography
           variant="h1"
           color={"white"}
+          fontWeight="400"
+          sx={{
+            zIndex: 10,
+            position: "absolute",
+            top: { xs: 80, sm: 110, md: 140, xl: 160 },
+            left: { xs: 15, sm: 25 },
+            fontSize: { xs: "3rem", sm: "3.5rem" },
+          }}
         >
-          <span className="text_bold_3">Compartilhe milhas,</span>
+          Compartilhe milhas,
           <br />
           <span className="text_bold_1">compartilhe o mundo.</span>
         </Typography>
         <Box
-          id="banner_front_home"
+          sx={{
+            zIndex: 1,
+            position: "absolute",
+            bottom: { xs: 120, xl: 80 },
+            right: { xs: 15, sm: 25 },
+            width: { xs: 270, sm: 370, md: 470, xl: 550 },
+            aspectRatio: "1.5/1",
+          }}
         >
           <Image
             src={frontBanner}
             alt="Imagem de uma garota com uma mochila de viagem nas costas."
+            style={{ width: "100%", height: "100%" }}
           />
         </Box>
       </Container>
